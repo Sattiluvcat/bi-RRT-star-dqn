55fa5f83318e85ba7cbc6d8fce7fc8bafca1eea4
Sattiluvcat <15059657+sattiluvcat@user.noreply.gitee.com>
2025-01-27 23:48:18 +0800

    vf-RRT生成与剪枝，新点选取未优化

--- Bi_RRT_star/single_planning/VF_Bi_RRT_star.py
+++ Bi_RRT_star/single_planning/VF_Bi_RRT_star.py
@@ -1,5 +1,6 @@
 # 类似Bi_RRT & Bi_RRT*子类，考虑流场情况下的rrt，新增生成新点、计算上游系数并选择最小cost路径的函数
 import numpy as np
+from numpy.lib.utils import source
 
 from single_planning.Bi_RRT_star import *
 from utils.node import Node
@@ -45,9 +46,9 @@ def get_vector_field(x, y, vector_field):
     x1 = int(np.floor(x)) + 20
     # 向量场的坐标系是从-220到20,U V的坐标系是从0开始
     y1 = int(np.floor(y)) + 220
-    x2 = x1 + 1
-    y2 = y1 + 1
-
+    # x2 = x1 + 1
+    # y2 = y1 + 1
+    # 这里不用改
     # u = U[y1, x1]*(x2-x) * (y2-y) + U[y1, x2]*(x-x1) * (y2-y) + U[y2, x1]*(x2-x) * (y-y1) + U[y2, x2]*(x-x1) * (y-y1)
     # v = V[y1, x1]*(x2-x) * (y2-y) + V[y1, x2]*(x-x1) * (y2-y) + V[y2, x1]*(x2-x) * (y-y1) + V[y2, x2]*(x-x1) * (y-y1)
     u = U[y1, x1]
@@ -84,8 +85,8 @@ def choose_lowest_cost(paths, vector_field):
     best_path = None
     for path in paths:
         cost = upstream_criterion(path, vector_field)
-        print("cost:", cost)
-        if cost < min_cost:
+        # print("cost:", cost)
+        if cost < min_cost and cost is not None:
             min_cost = cost
             best_path = path
     return best_path
@@ -93,25 +94,31 @@ def choose_lowest_cost(paths, vector_field):
 
 # 考虑流场的剪枝
 def vf_prune_path(path, obs_list, vector_field):
+    mini_degree = 90  # 最大转角，180°-mini_degree，反着定义
     pruned_path = [path[0]]
     i = 0
 
     while i < len(path) - 1:
         found = False
         for j in range(len(path) - 1, i, -1):
-            # TODO 转角约束
+            # 转角约束
+            if i > 0:
+                # 这里转角之前定义错了
+                degree = calc_triangle_deg(path[i], path[i-1], path[j])
+                if degree < mini_degree and degree != 0:
+                    continue
+            # 碰撞约束
             if not check_collision(path[i], path[j], obs_list):
-                # 现有优化路径+现在考虑的路径不剪枝形式
-                candidate_path = pruned_path + path[i:]
-
+                # 现有优化路径 + 现在考虑的路径不剪枝形式
+                candidate_path = pruned_path + path[i + 1:]
                 # 计算路径评分——跳过 i 到 j 中间的路径
-                score_start_to_current = path_score(pruned_path + path[j:], vector_field)
-                # 计算路径评分——已优化路径
-                score_current_to_previous = path_score(candidate_path, vector_field)
-                print("score_start_to_current:", score_start_to_current,
-                      " score_current_to_previous:", score_current_to_previous)
-                # Compare the scores
-                if score_start_to_current < score_current_to_previous:
+                score_jump = path_score(pruned_path + path[j:], vector_field)
+                # 计算路径评分——不跳
+                score_old = path_score(candidate_path, vector_field)
+                print("score_jump:", score_jump, " score_old:", score_old)
+                print("pruned_path:", pruned_path, " i:", i)
+                # Compare the scores,分数越小越好
+                if score_jump - score_old<=0 and score_jump != 0:
                     pruned_path.append(path[j])
                     i = j
                     found = True
@@ -125,20 +132,53 @@ def vf_prune_path(path, obs_list, vector_field):
 
 # 剪枝中的评分函数
 def path_score(path, vector_field):
+    print("score path:", path)
+    # 累计diff 向量场方向与路径方向的差异
     total_difference = 0
+    # 累计转角
     total_angle = 0
-    # TODO 评分细则
+    # 累计长度
+    total_length = 0
+    # 评分细则（+转角考虑）
     for i in range(len(path) - 1):  # 最大值是 len(path) - 2
-        u, v = get_vector_field(path[i][0], path[i][1], vector_field)
-        direction_vector_field = np.arctan2(v, u)
-        direction_path = np.arctan2(path[i + 1][1] - path[i][1], path[i + 1][0] - path[i][0])
-        total_difference += abs(direction_path - direction_vector_field)
+        start_point = path[i]
+        end_point = path[i + 1]
 
+        # 计算路径段的距离
+        distance = np.linalg.norm(np.array(end_point) - np.array(start_point))
+        total_length += distance
+        # 根据距离确定点的数量，向量场为 1m x 1m 的网格
+        num_points = int(distance) + 1
+        # 路径段上的每个点作区划，得到累计diff
+        x_values = np.linspace(start_point[0], end_point[0], num_points)
+        y_values = np.linspace(start_point[1], end_point[1], num_points)
+        x_values = x_values[:num_points - 1]
+        y_values = y_values[:num_points - 1]
+        for x, y in zip(x_values, y_values):
+            u, v = get_vector_field(x, y, vector_field)
+            if np.isnan(u) or np.isnan(v):
+                continue
+            direction_vector_field = np.arctan2(v, u)
+            direction_path = np.arctan2(end_point[1] - start_point[1], end_point[0] - start_point[0])
+            total_difference += abs(direction_path - direction_vector_field)
+            # if np.isnan(total_difference):
+            #     print("u:", u, " v:", v, " direction_vector_field:", direction_vector_field, " direction_path:",
+            #           direction_path, " start:", start_point, " end:", end_point)
+        # print("total_difference:", total_difference)
         if i < len(path) - 2:
             angle = abs(np.arctan2(path[i + 2][1] - path[i + 1][1], path[i + 2][0] - path[i + 1][0]) - direction_path)
             total_angle += angle
+        # print("total_angle:", total_angle)
+        # 归一化并赋权重
+        # total_length /= 350
+        # total_difference /= 150
+        # total_angle /= 20
+        prize_length = 0.1
+        prize_difference = 1
+        prize_angle = 0.1
 
-    return total_difference + total_angle
+    print("total_length:", total_length, " total_difference:", total_difference, " total_angle:", total_angle)
+    return total_difference * prize_difference + total_angle * prize_angle + total_length * prize_length
 
 
 def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
@@ -151,7 +191,7 @@ def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
     obs_list = obslis_xy
     extend_length = 5
     max_iter = 10000
-    mini_degree=90      # 最大转角，180°-mini_degree，反着定义
+    mini_degree = 90  # 最大转角，180°-mini_degree，反着定义
     # 路径总数
     path_num = 5
 
@@ -169,6 +209,7 @@ def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
     path_found = False
 
     while path_num > 0:
+        path_found=False
         for i in range(max_iter):
             rnd_nd1 = get_random_node(x_min, x_max, y_min, y_max, goal_point)
             rnd_nd2 = get_random_node(x_min, x_max, y_min, y_max, start_point)
@@ -231,7 +272,9 @@ def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
                     while node:
                         path2.append([node.x, node.y])
                         node = node.parent
-                    paths.append(path1 + path2)
+                    path=path1+path2
+                    print("上个路径：",path)
+                    paths.append(path)
                     # path_num -= 1
                     break
 
@@ -243,13 +286,17 @@ def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
                     while node:
                         path2.append([node.x, node.y])
                         node = node.parent
-                    path2.reverse()
+                    # path2.reverse()
                     path1 = []
-                    node = new_nd2
+                    node = new_nd1
                     while node:
-                        path2.append([node.x, node.y])
+                        path1.append([node.x, node.y])
                         node = node.parent
-                    paths.append(path2+path1)
+                    path1.reverse()
+                    path = path1 + path2
+                    # path.reverse()
+                    paths.append(path)
+                    print("这个路径 ",path)
                     # path_num -= 1
                     break
 
@@ -260,6 +307,8 @@ def VF_Bi_RRT_star_plan(start_xy, goal_xy, obslis_xy, vector_field):
 
     if paths:
         best_path = choose_lowest_cost(paths, vector_field)
+        print("best_path:", best_path)
         pruned_path = vf_prune_path(best_path, obs_list, vector_field)
         return pruned_path
+        # return best_path
     return None
